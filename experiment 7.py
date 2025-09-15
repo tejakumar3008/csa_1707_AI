9137f1b263127ef0fc04a6f88a9dcab05ea9650e
tejakumar3008 <tk3909688@gmail.com>
2025-09-15 10:34:44 +0530

    Create experiment 7.py

---
+++ experiment 7.py
@@ -0,0 +1,20 @@
+from collections import deque
+def bfs(graph, start):
+    visited = set()         
+    queue = deque([start]) 
+    print("BFS Traversal: ", end="")
+    while queue:
+        vertex = queue.popleft()
+        if vertex not in visited:
+            print(vertex, end=" ")
+            visited.add(vertex)
+            queue.extend(neighbor for neighbor in graph[vertex] if neighbor not in visited)
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
+}
+bfs(graph, 'A')
